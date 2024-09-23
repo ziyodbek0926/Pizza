17dbed2971f016efcd06c0b1ca80fbcf710fe40c
pepperal <pepperal@student.21-school.ru>
2024-09-23 10:24:42 +0500

    access

---
+++ globals.py
@@ -0,0 +1,113 @@
+WELCOME_TEXT = "Xush kelibsiz / Добро пожаловать!"
+CHOOSE_LANG = "Tilni tanlang / Выберите язык!"
+BTN_LANG_UZ = "Uzbek tili"
+BTN_LANG_RU = "Русский язык"
+STATES = {
+    "reg": 1,
+    "menu": 2,
+    "settings": 3
+}
+LANGUAGE_CODE = {
+    1: "uz",
+    2: "ru"
+}
+TEXT_LANG_WARNING = "Iltimos tillardan birini tanlang!\nПожалуйста, выберите один из языков!"
+TEXT_ENTER_FIRST_NAME = {
+    1: "Iltimos ismingizni kiriting!",
+    2: "Пожалуйста, введите ваше имя!"
+}
+TEXT_ENTER_LAST_NAME = {
+    1: "Iltimos familiyangizni kiriting!",
+    2: "Пожалуйста, введите свою фамилию!"
+}
+BTN_SEND_CONTACT = {
+    1: "Yuborish",
+    2: "Отправить"
+}
+TEXT_ENTER_CONTACT = {
+    1: "Iltimos telefon raqamingizni yuboring yoki <<Yuborish>> tugmasini bosing!",
+    2: "Отправьте свой номер телефона или нажмите кнопку <<Отправить>>!"
+}
+TEXT_MAIN_MENU = {
+    1: "🏠 Asosiy menyu",
+    2: "🏠 Главное меню"
+}
+BTN_ORDER = {
+    1:"🛒 Buyurtma qilish",
+    2:"🛒 Заказать"
+}
+TEXT_ORDER = {
+    1: "Kategoriylardan birini tanlang!",
+    2: "Выбери одну из категорий!"
+}
+BTN_MY_ORDERS = {
+    1:"🛍 Buyurtmalarim",
+    2:"🛍 Мои заказы"
+}
+BTN_ABOUT_US = {
+    1:"👨‍👩‍👦 Biz haqimizda",
+    2:"👨‍👩‍👦 O нас"
+}
+BTN_KORZINKA = {
+    1: "🛍 Xarid qilish",
+    21: "🛍 Покупка товаров",
+}
+BTN_COMMENTS = {
+    1:"✍️Fikr bildirish",
+    2:"✍️комментарий"
+}
+BTN_SETTINGS = {
+    1:"⚙️Sozlamalar",
+    2:"⚙️Настройки"
+}
+
+TEXT_PRODUCT_PRICE = {
+    1: "Narx:",
+    2: "Цена:"
+}
+TEXT_PRODUCT_DESC = {
+    1: "Tarkibi:",
+    2: "Состав:"
+}
+
+
+
+AT_KORZINKA = {
+    1: "Savatchada",
+    2: "В корзине"
+}
+
+ALL = {
+    1: "Jami",
+    2: "Все"
+}
+
+ZAKAZ = {
+    1: "Buyurtma",
+    2: "Заказ"
+}
+
+SUM = {
+    1: "so'm",
+    2: "сум"
+}
+
+ABOUT_COMPANY = {
+    1: "Biz mazzali taomlar tayyorlaymiz."
+         "\n\nBiz bilan bog'lanish uchun: +99893-051-47-37"
+         "Bizning manzil: \n\n",
+    2: "Готовим вкусную еду"
+       "\n\nДля связи с нами: +99893-051-47-37"
+       "Наш адрес:\n\n"
+       
+}
+
+NO_ZAKAZ = {
+    1: "Siz hali birorta buyurtma bermadingiz!",
+    2: "Вы еще не разместили ни одного заказа!"
+}
+
+SEND_LOCATION = {
+    1: "📍 Lokatsiya jo'nating",
+    2: "📍 Отправить местоположение"
+}
